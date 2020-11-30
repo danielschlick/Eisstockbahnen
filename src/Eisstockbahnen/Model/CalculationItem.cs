40de8064c1de37716c90986851392afec695c1c7
Daniel Schlick <d.schlick@outlook.com>
2020-11-30 20:00:36 +0100

    frontend changes

---
+++ src/Eisstockbahnen/Model/CalculationItem.cs
@@ -0,0 +1,29 @@
+﻿using EisstockbahnenWebModel;
+
+namespace Eisstockbahnen.Model
+{
+    public class CalculationItem
+    {
+        public int Bought { get; set; }
+        public int Sold { get; set; }
+
+        public double BuyPrice { get; set; }
+        public double SellPrice { get; set; }
+
+        public ProductModel Product { get; set; }
+
+        public CalculationItem(ProductModel product) : this(0, 0, 0, 0, product)
+        {
+        }
+
+        public CalculationItem(int bought, int sold, double buyPrice, double sellPrice, ProductModel product)
+        {
+            Bought = bought;
+            Sold = sold;
+
+            BuyPrice = buyPrice;
+            SellPrice = sellPrice;
+            Product = product;
+        }
+    }
+}
