8e94e8871a09fe8eefcfc4424c0230b90fe15f48
Daniel Schlick <51923198+danielschlick@users.noreply.github.com>
2020-12-13 18:06:02 +0100

    design fronted for all pages, add images

---
+++ src/Eisstockbahnen/wwwroot/js/app.js
@@ -0,0 +1,17 @@
+﻿function initialize() {
+    var latlng = new google.maps.LatLng(48.369060, 13.820479);
+    new google.maps.Marker({
+        position: latlng,
+        map,
+        title: "Eisstockbahnen Ratzenböck!",
+    });
+
+    var options = {
+        zoom: 14,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+
+    var map = new google.maps.Map(document.getElementById("map"), options);
+}   
