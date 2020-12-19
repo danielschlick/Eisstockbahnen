4d8295fca8587a566c3e742206aa404ba5599c3a
Daniel Schlick <51923198+danielschlick@users.noreply.github.com>
2020-12-19 16:47:13 +0100

    implement authentication page, update history text, bug fixes

--- src/Eisstockbahnen/wwwroot/js/app.js
+++ src/Eisstockbahnen/wwwroot/js/app.js
@@ -15,3 +15,35 @@
 
     var map = new google.maps.Map(document.getElementById("map"), options);
 }   
+
+function modelOpen() {
+    
+}
+
+window.javaScriptFunction = {
+    showModal: function () {
+        document.addEventListener('DOMContentLoaded', function () {
+            var Modalelem = document.querySelector('.modal');
+            var instance = M.Modal.init(Modalelem);
+            instance.open();
+        });
+    },
+
+    showAlert: function (test) {
+        alert(test);
+    },
+    //displayWelcome: function (welcomeMessage) {
+    //    document.getElementById('welcome').innerText = welcomeMessage;
+    //},
+    //returnArrayAsyncJs: function () {
+    //    DotNet.invokeMethodAsync('BlazorWebAssemblySample', 'ReturnArrayAsync')
+    //        .then(data => {
+    //            data.push(4);
+    //            console.log(data);
+    //        });
+    //},
+    //sayHello: function (dotnetHelper) {
+    //    return dotnetHelper.invokeMethodAsync('SayHello')
+    //        .then(r => console.log(r));
+    //}
+};
